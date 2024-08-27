007e73072e55b84c362095bbd9091e11360d0a7f
SenudiGamage2003 <senudigamage2003@gmail.com>
2024-08-27 13:49:30 +0530

    settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,8 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
+  MONGODB_URL: '
+mongodb+srv://ayeshrashmina2005:bZAHiCLVhd9DZQ0O@cluster0.xp2mp.mongodb.net/', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
